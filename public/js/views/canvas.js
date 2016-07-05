723d011bd34bb208fa6463fcdddaaa6cc5e13c2c
Nafisa <murkyspring@gmail.com>
2016-07-05 14:34:12 -0500

    Modularized three.js code and contained it within CanvasView.

--- public/js/views/canvas.js
+++ public/js/views/canvas.js
@@ -4,43 +4,41 @@ $(function () {
 	
   app.CanvasView = Backbone.View.extend({
   	
-    el: '#webglcanvas',
+    el: "#webglcanvas",
 
     initialize: function() {
-      this.draw();
-      this.listenTo(this.model, 'change', this.render);
+	
+	this.scene = new THREE.Scene();
+	this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 20000);
+
+	this.renderer = new THREE.WebGLRenderer({antialias:true});
+	this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+	this.$el.append(this.renderer.domElement);
+	this.geometry = new THREE.BoxGeometry(1,1,1);
+	this.material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+	this.cube = new THREE.Mesh(this.geometry, this.material);
+	this.scene.add(this.cube);
+	this.camera.position.z = 5;
+	
+	this.animate();
+
+	this.listenTo(this.model, 'change', this.animate);
     },
 
-    draw: function() {
-  	var scene = new THREE.Scene();
-			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    animate: function() {
+	var parent = this;
+	requestAnimationFrame(function() {parent.animate();});
 
-			var renderer = new THREE.WebGLRenderer();
-			renderer.setSize( window.innerWidth, window.innerHeight );
-			document.body.appendChild( renderer.domElement );
-
-			var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-			var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			var cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
-
-			camera.position.z = 5;
-
-			var render = function () {
-				setTimeout(requestAnimationFrame( render ), 300);
-
-				cube.rotation.x += 0.1;
-				cube.rotation.y += 0.1;
-
-				renderer.render(scene, camera);
-			};
-
-			render();
+	this.cube.rotation.x -= 0.05;
+	this.cube.rotation.y -= 0.05;
+	this.renderer.render(parent.scene, parent.camera);
     },
 
     render: function() {
     	//var template = _.template($('#three').html());
     	//this.$el.html( template({ name : this.model.get('data')[String(this.model.get('x'))][this.model.get('y')]}) );
+	
     }
 
   });
